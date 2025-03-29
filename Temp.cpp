23349beb584fc8adad63aef440deaeed91bc2e55
DnyaneshParate <dnyaneshparate16@gmail.com>
2025-03-29 11:34:06 +0530

    C++ file is been added

--- Temp.cpp
+++ Temp.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main (){
     cout<<"This Is the file which is been added in the Giithub\n";
+    int num;
+    cin>>num;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<num <<" X "<< i << " = "<< num*i<<endl;
+    }
     return 0;
 }
